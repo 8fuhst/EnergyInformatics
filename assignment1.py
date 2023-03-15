0f465db936c16f9fd5095155114d4bde5d02bc11
Leonard Rupietta <leonard@rupietta.name>
2023-03-15 02:12:51 +0100

    Adjusted Non-shiftable tasks and fixed plot for task 2

--- assignment1.py
+++ assignment1.py
@@ -41,6 +41,60 @@ def flatten(lst: list):
     return [item for sublist in lst for item in sublist]
 
 
+def create_usage_plots_task_2(lst: list):
+    """Draws a stacked bar chart for the use of household appliances in a neighborhood."""
+    variables_list = [[item[0] for item in items] for items in lst]
+    flat_list = flatten(variables_list)
+    count_dict = {}
+    ev = np.zeros(24)
+    vacuum = np.zeros(24)
+    dryer = np.zeros(24)
+    laundry = np.zeros(24)
+    dishwasher = np.zeros(24)
+    hairdryer = np.zeros(24)
+    microwave = np.zeros(24)
+    phone = np.zeros(24)
+    for l in flat_list:
+        if l not in count_dict.keys():
+            count_dict[l] = flat_list.count(l)
+    for k, v in count_dict.items():
+        _, k_2 = k.split("_")
+        k_2 = int(k_2)
+        if 'electric' in k.lower():
+            ev[k_2] = v
+        elif 'vacuum' in k.lower():
+            vacuum[k_2] = v
+        elif 'dryer' in k.lower():
+            dryer[k_2] = v
+        elif 'laundry' in k.lower():
+            laundry[k_2] = v
+        elif 'dish' in k.lower():
+            dishwasher[k_2] = v
+        elif 'microwave' in k.lower():
+            microwave[k_2] = v
+        elif 'hair' in k.lower():
+            hairdryer[k_2] = v
+    values = {
+        "EV": ev,
+        "Vacuum": vacuum,
+        "Dryer": dryer,
+        "Laundry Machine": laundry,
+        "Dishwasher": dishwasher,
+        "Microwave": microwave,
+        "Hairdryer": hairdryer
+    }
+    bottom = np.zeros(24)
+
+    for name, count in values.items():
+        plt.bar([x for x in range(24)], count, label=name, bottom=bottom)
+        bottom += count
+
+    plt.xticks([x for x in range(24)])
+    plt.title("Amount of Appliances used at a Given Time")
+    plt.legend(loc="upper right")
+    plt.show()
+
+
 def create_usage_plots_task_2_3(lst: list):
     """Draws a stacked bar chart for the use of household appliances in a neighborhood."""
     variables_list = [[item[0] for item in items] for items in lst]
@@ -152,7 +206,7 @@ def assignment_2():
     problem.addConstraint(lpSum(m[i] for i in hours) == 0.6), 'Microwave Constraint'
 
     problem.solve()
-    create_usage_plots_task_2_3([create_input(problem)])
+    create_usage_plots_task_2([create_input(problem)])
 
     print(value(problem.objective))
     # TODO: Consider non-shiftables, add their price according to price curve
@@ -161,6 +215,8 @@ def assignment_2():
 def assignment_3():
     prices = [4,  6,  3,  5,  7,  7,  6,  5,  4,  4,  6,  5,  3,  6,  7,  7,  3, 14, 16, 16,  4,  6,  3,  6]
     hours = [x for x in range(0, 24)]
+    
+    
     total = 0
     EV_CHANCE = 0.66  # Assume that 2/3rds of households have EV
     output_dict = {}
@@ -213,34 +269,42 @@ def assignment_3():
         print(value(problem.objective))
 
     values = output_dict.values()
+    print(values)
     amount_evs = sum('EV' in item for item in values)
+    Vacuum = sum('Vacuum' in item for item in values)
+    Microwave = sum('Microwave' in item for item in values)
+    HairDryer = sum('HairDryer' in item for item in values)
+    PhoneCharger = sum('PhoneCharger' in item for item in values)
+    CeilingFan = sum('CeilingFan' in item for item in values)
     create_usage_plots_task_2_3(input_list)
-    print(f"Amount of EVs: {amount_evs}")
+    print(f"Amount of EVs: {amount_evs}, Vacuum {Vacuum}, HairDryer {HairDryer} PhoneCharger {PhoneCharger} CeilingFan {CeilingFan} Microwave {Microwave}")
     print(f"Share of electric vehicles: {amount_evs/30}")
     print(f"Total price of all households for the day: {total}")  # Total price of all households over the day
     # TODO: Consider non-shiftables, add their price according to price curve
     # Since we don't have to consider grid load, every household can use the same appliances at the same time
     # to save money (everyone uses the optimal solution).
 
-def non_shiftables_task_4():
+def non_shiftables_task_basic():
     prices = [4, 6, 3, 5, 7, 7, 6, 5, 4, 4, 6, 5, 3, 6, 7, 7, 3, 14, 16, 16, 4, 6, 3, 6]
     consumption_per_hour = [0.0 for x in range(0, 24)]
     price_for_non_shiftable = [0.0 for x in range(0, 24)]
     # price for non-shiftable devices are fixed that the price for the non-shiftable devices
     # is the same in task 2 and 4
     lightning = 1.5/10 # usage per hour between 10.00 - 20.00
-    heating = 8/24
-    refrigerator = 2.64/24 # household has two refigerators
-    electric_stove = 3.9 /24
+    heating = 8/24 # 24/7
+    refrigerator = 2.64/24 #24/7 household has two refigerators
+    electric_stove = 3.9 /2 # on 18 and 19
     tv = 0.4 /5  # in this example tv is on between 18.00 - 23.00
-    computers = 1.8/24 # in this task 3 computers
+    computers = 1.8/5 # in this task 3 computers, are on between 18.00 - 23.00
 
     for i in range(len(consumption_per_hour)):
-        fixed_consumption_per_hour = heating + electric_stove + computers + refrigerator
+        fixed_consumption_per_hour = heating + refrigerator
         if 9 <= i <= 19:
             fixed_consumption_per_hour = fixed_consumption_per_hour + lightning
         if 17 <= i <= 22:
-            fixed_consumption_per_hour = fixed_consumption_per_hour + tv
+            fixed_consumption_per_hour = fixed_consumption_per_hour + tv + computers
+        if 18 <= i <= 19:
+            fixed_consumption_per_hour = fixed_consumption_per_hour + electric_stove
 
         consumption_per_hour[i] = fixed_consumption_per_hour
         price_for_non_shiftable[i] = prices[i] * fixed_consumption_per_hour
@@ -254,7 +318,7 @@ def non_shiftables_task_4():
 
 
 def assignment_4():
-    non_shiftable_consumption, non_shiftable_prices = non_shiftables_task_4()
+    non_shiftable_consumption, non_shiftable_prices = non_shiftables_task_basic()
     prices = [4, 6, 3, 5, 7, 7, 6, 5, 4, 4, 6, 5, 3, 6, 7, 7, 3, 14, 16, 16, 4, 6, 3, 6]
     problem = LpProblem('Demand Response', LpMinimize)
     hours = list(range(24))
@@ -290,5 +354,5 @@ def assignment_4():
 
 
 if __name__ == '__main__':
-    assignment_4()
-    # non_shiftables_task_4()
+    assignment_3()
+    # non_shiftables_task_basic()
